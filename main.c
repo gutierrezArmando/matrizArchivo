bfdf5f4853082c27cd38332f4431261360207089
José Armando Gutiérrez <gutierrezjarr@gmail.com>
2018-03-30 15:33:34 -0800

    Finalizacion
    El programa funcional
    
    el línea de comando capturar sin espacios: "1,2,34,56,9"

--- main.c
+++ main.c
@@ -1,16 +1,65 @@
 #include <stdio.h>
 #include <string.h>
 
+void capturaFila(char *cadena);
+void enviarAlArchivo(FILE* archivo, char *cadenaP1, char *cadenaDeDatos);
+void rowToFile(FILE* archivo, char *cadena);
+
 int main() {
     FILE* archivo = fopen("demo.txt","w+b");
-    char cadena[25];
+    char nombreMatriz[20];
+    char cadenaDatos[50];//Dode se captura los datos en linea de comando
+    char cadena[50];//Parte inicia de la cadena que se colocará en el archivo
     int i=0;
-    while(cadena[i])
-        fwrite(&cadena[i++],sizeof(char),1, archivo);
+
+    printf("Nombre de la matriz: ");
+    scanf("%s", nombreMatriz);
+    do {
+        sprintf(cadena, "%s[%d]", nombreMatriz, i++);
+        capturaFila(cadenaDatos);
+        enviarAlArchivo(archivo, cadena, cadenaDatos);
+        printf("\nCapturar otra: s=SI, ENTER=terminar\n");
+    } while(getch()=='s');
     fclose(archivo);
-    printf("Programa finalizado");
     return 0;
 }
 
-// c = '[';
-// fwrite(&c, sizeof(char),1,archivo);
+void capturaFila(char *cadena)
+{
+    printf("\nIngresa los valores se parados por comas, Ejemplo: 0,1,2,a,b,f,23,7\nValores: ");
+    scanf("%s", cadena);
+}
+
+
+void enviarAlArchivo(FILE* archivo, char *cadenaP1, char *cadenaDatos){
+
+    int i=0;
+    int valor=0, ultimoValor=0;
+    char lineOnFile[50];
+    while (*cadenaDatos) {
+        if(*cadenaDatos!=','){
+            if(valor){
+                valor*=10;
+                valor+=(*cadenaDatos) - '0';
+            }
+            else
+                valor = (*cadenaDatos) - '0';
+            ultimoValor= valor;
+        }
+        else
+        {
+            sprintf(lineOnFile,"%s[%d]=%d\n", cadenaP1,i++, valor);
+            rowToFile(archivo, lineOnFile);
+            valor=0;
+        }
+        *cadenaDatos++;
+    }
+    sprintf(lineOnFile, "%s[%d]=%d\n", cadenaP1,i++, ultimoValor);
+    rowToFile(archivo, lineOnFile);
+}
+
+void rowToFile(FILE* archivo, char *cadena){
+    int i=0;
+    while(cadena[i])
+        fwrite(&cadena[i++],sizeof(char),1, archivo);
+}
